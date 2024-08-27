78deca09761ad7852ea9336e4089ba7c2619901f
Diego Pincer <diegopincer@hotmail.com>
2024-08-27 03:26:31 +0200

    Bug Fix

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
 using namespace cimg_library;
  
 int main() {
-  CImg<unsigned char> image("src/lenna.png"), visu(500,400,1,3,0);
+  CImg<unsigned char> image("lenna.png"), visu(500,400,1,3,0);
   const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
   image.blur(2.5);
   CImgDisplay main_disp(image,"Click a point"), draw_disp(visu,"Intensity profile");
